4f2a08ad7bf1fd85c9846b4725ea383ca240480f
Marc Zyngier <maz@kernel.org>
2021-12-15 17:45:21 +0000

    KVM: arm64: Use local TLBI on permission relaxation
    Broadcasted TLB invalidations (TLBI) are usually less performant than their
    local variant. In particular, we observed some implementations that take
    millliseconds to complete parallel broadcasted TLBIs.
    
    It's safe to use local, non-shareable, TLBIs when relaxing permissions on a
    PTE in the KVM case for a couple of reasons. First, according to the ARM
    Arm (DDI 0487H.a D5-4913), permission relaxation does not need
    break-before-make.  Second, KVM does not set the VTTBR_EL2.CnP bit, so each
    PE has its own TLB entry for the same page. KVM could tolerate that when
    doing permission relaxation (i.e., not having changes broadcasted to all
    PEs).
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Ricardo Koller <ricarkol@google.com>

--- arch/arm64/kvm/hyp/pgtable.c
+++ arch/arm64/kvm/hyp/pgtable.c
@@ -1147,7 +1147,7 @@ int kvm_pgtable_stage2_relax_perms(struct kvm_pgtable *pgt, u64 addr,
 	ret = stage2_update_leaf_attrs(pgt, addr, 1, set, clr, NULL, &level,
 				       KVM_PGTABLE_WALK_SHARED);
 	if (!ret)
-		kvm_call_hyp(__kvm_tlb_flush_vmid_ipa, pgt->mmu, addr, level);
+		kvm_call_hyp(__kvm_tlb_flush_vmid_ipa_nsh, pgt->mmu, addr, level);
 	return ret;
 }
 
